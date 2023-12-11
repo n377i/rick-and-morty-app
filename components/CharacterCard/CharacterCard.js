5e462d691c3ad593640a354f0c5eb9275cde1ab4
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-11 10:40:39 +0100

    + add pagination

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -14,7 +14,7 @@ export default function CharacterCard(character) {
         <h2 class="card__title">${character.name}"</h2>
         <dl class="card__info">
           <dt class="card__info-title">Status</dt>
-          <dd class="card__info-description">${character.status}/dd>
+          <dd class="card__info-description">${character.status}</dd>
           <dt class="card__info-title">Type</dt>
           <dd class="card__info-description">${character.type}</dd>
           <dt class="card__info-title">Occurrences</dt>
--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 42;
+let page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
@@ -26,7 +26,9 @@ async function fetchCharacters() {
     }
 
     const data = await response.json();
+    maxPage = data.info.pages;
     const characters = data.results;
+    pagination.textContent = `${page} / ${maxPage}`;
     cardContainer.innerHTML = "";
     characters.map(CharacterCard).forEach((card) => cardContainer.append(card));
   } catch (error) {
@@ -35,3 +37,17 @@ async function fetchCharacters() {
 }
 
 fetchCharacters();
+
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+});
