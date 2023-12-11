5347917730188f657abf68fefe6b99a73ae51e43
Annette Wöhler <annettewoehler@gmx.de>
2023-12-11 10:16:37 +0100

    + Add character card and fetch data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,24 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page=${page}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Network error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const characters = data.results;
+    cardContainer.innerHTML = "";
+    characters.map(CharacterCard).forEach((card) => cardContainer.append(card));
+  } catch (error) {
+    console.error("Error fetching characters:", error.message);
+  }
+}
+
+fetchCharacters();
